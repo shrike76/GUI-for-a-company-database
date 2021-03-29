fda3f535a145935b6a4e1e7e17ce017613106463
crkiel <60700977+crkiel@users.noreply.github.com>
2021-03-28 22:47:48 -0500

    The rough draft for the the login and connection to the database.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,63 @@
 package sample;
 
-public class Controller {
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+import java.awt.*;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    //Login window
+    //Username txtfield
+    @FXML
+    private TextField UsernameBox;
+    //Password txtfield
+    @FXML
+    private TextField PasswordBox;
+    //Login Button
+    @FXML
+    private Button Log;
+
+    ///database info (rough placeholder)
+    final String hostname = "";
+    final String dbname = "";
+    final String port = "";
+    final String username = "";
+    final String password = "";
+    //URL to AWS Database
+    final String AWS = "jdbc:mysql://" + hostname + ":" + port + "/" + dbname + "?user=" + username + "&password=" + password;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        Log.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+                try
+                {
+                    Connection conn = DriverManager.getConnection(AWS);
+                    Statement stmt = conn.createStatement();
+                    System.out.println("CONNECTED");
+                }
+                catch(Exception ex)
+                {
+                    System.out.println("ERROR: " + ex.getMessage());
+                }
+
+            }
+        });
+
+    }
 }
